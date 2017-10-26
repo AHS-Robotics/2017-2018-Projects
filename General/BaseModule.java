ea3af6a3fce022592176334edeb211d3a1097401
BenTibnam <BenTibnam@users.noreply.github.com>
2017-10-26 01:35:59 -0400

    stupidly erased entire BaseModule for a more flexable model

--- General/BaseModule.java
+++ General/BaseModule.java
@@ -3,123 +3,127 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import org.firstinspires.ftc.teamcode.LinearOpModePlus;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.LinearOpModePlus;
 
-enum Diagonal{
-    RIGHT_FWRD, RIGHT_BACK, LEFT_FWRD, LEFT_BACK
-}
-
-@TeleOp(name="Base Module 1.0.0", group="Building Blocks")
 public class BaseModule extends LinearOpModePlus{
-    private final double SLEEP_TIME = 1000;
-    private double power = 0;
-    private double lXAxis, lYAxis;
-    private double rXAxis, rYAxis;
-
-    private DcMotor motorLeftBack;
     private DcMotor motorLeftFront;
-    private DcMotor motorRightBack;
+    private DcMotor motorLeftBack;
     private DcMotor motorRightFront;
-    
-    private boolean testDiagonal = true;
-    private boolean updateTele = true;
-    
-    // all of the 'turnX' methods will be defined later
-    public void turnLeft(){
-        try{
-            motorLeftFront.setPower(MAX_CAP * multiplier);
-            motorLeftBack.setPower(MAX_CAP * multiplier);
-            Thread.sleep(SLEEP_TIME);
-            motorLeftFront.setPower(0);
-            motorLeftBack.setPower(0);
-            
-        }catch(InterruptedException e){
-            // we'll do something here but not now
-        }
-    }
-    
+    private DcMotor motorRightBack;
+
+    private double multiplierStorage = 0;
+    private float lYAxis = 0, lXAxis = 0;
+
+    private boolean leftRev = true; // if the value changes during updates different wheels will be reversed
+    private boolean suspendAllMotors = false;
+
+
+    /**
+     * At the start of every run, variable information will be updated */
     public void update(){
-        // This always has to run
         lYAxis = gamepad1.left_stick_y;
-        lXAxis = gamepad1.left_stick_y;
-    }
-    
-    public void runDiagonal(Diagonal d){
-        switch(d){
-            case Diagonal.LEFT_FWRD:
-                motorLeftBack.setPower(-gamepad1.left_stick_x * multiplier);
-                motorRightFront.setPower(-gamepad1.left_stick_x * multiplier);
-                motorLeftFront.setPower(-gamepad1.left_stick_y * multiplier);
-                motorRightBack.setPower(-gamepad1.left_stick_y * multiplier);
-                break;
-            case Diagonal.LEFT_BACK:
-                motorLeftFront.setPower(-gamepad1.left_stick_x * multiplier);
-                motorRightBack.setPower(-gamepad1.left_stick_x * multiplier);
-                motorLeftBack.setPower(-gamepad1.left_stick_y * multiplier);
-                motorRightFront.setPower(-gamepad1.left
-                break;
-            case Diagonal.RIGHT_FWRD:
-                motorLeftFront.setPower(-gamepad1.left_stick_x * multiplier);
-                motorRightBack.setPower(-gamepad1.left_stick_x * multiplier);
-                motorLeftBack.setPower(-gamepad1.left_stick_y * multiplier);
-                motorRightFront.setPower(-gamepad1.left_stick_y * multiplier);
-                break;
-            case Diagonal.RIGHT_BACK:
-                motorLeftBack.setPower(-gamepad1.left_stick_x * multiplier);
-                motorRightFront.setPower(-gamepad1.left_stick_x * multiplier);
-                motorLeftFront.setPower(-gamepad1.left_stick_y * multiplier);
-                motorRightBack.setPower(-gamepad.left_stick_y * multiplier);
-                break;
-            default:
-                // we'll print an error message
-                break;
+        lXAxis = gamepad1.left_stick_x;
+
+        if(leftRev){
+            motorLeftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+            motorLeftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+            motorRightBack.setDirection(DcMotorSimple.Direction.FORWARD);
+            motorRightFront.setDirection(DcMotorSimple.Direction.FORWARD);
+        }else{
+            motorLeftBack.setDirection(DcMotorSimple.Direction.FORWARD);
+            motorLeftFront.setDirection(DcMotorSimple.Direction.FORWARD);
+            motorRightBack.setDirection(DcMotorSimple.Direction.REVERSE);
+            motorRightFront.setDirection(DcMotorSimple.Direction.REVERSE);
         }
+
+        // This is experimental so tests are needed
+        if(suspendAllMotors && multiplierStorage <= 0) {
+            multiplierStorage = multiplier;
+            multiplier = 0;
+        }else if(!suspendAllMotors && multiplierStorage > 0){
+            multiplier = multiplierStorage;
+            multiplierStorage = 0;
+        }
+
+    }
+
+    private void turn(char dir){
+        if((dir != 'L' || dir != 'l') || (dir != 'R' || dir != 'r') )return; // here we need to print an error but that has to wait until we print functions work better
+        else{
+            dir = Character.toUpperCase(dir);
+            DcMotor motorsWithChange[] = new DcMotor[2]; // this are the ones we'll have to reverse
+            DcMotorSimple.Direction rev, fwd;
+
+            if(dir == 'L') {
+                motorsWithChange[0] = motorLeftBack;
+                motorsWithChange[1] = motorLeftFront;
+                rev = (leftRev) ? DcMotorSimple.Direction.FORWARD : DcMotorSimple.Direction.REVERSE;
+                fwd = (leftRev) ? DcMotorSimple.Direction.REVERSE : DcMotorSimple.Direction.FORWARD;
+            }else if(dir == 'R'){
+                motorsWithChange[0] = motorRightBack;
+                motorsWithChange[1] = motorLeftFront;
+                rev = (leftRev) ? DcMotorSimple.Direction.REVERSE : DcMotorSimple.Direction.FORWARD;
+                fwd = (leftRev) ? DcMotorSimple.Direction.FORWARD : DcMotorSimple.Direction.REVERSE;
+            }else return; // this should never happen just has to be here so we can define use defined variables
+
+            for(DcMotor m : motorsWithChange) m.setDirection(rev); // setting the reverse directions of motors
+
+            while(((dir == 'L' && dir != 'R') ? gamepad1.left_trigger:gamepad1.right_trigger) > 0){
+                motorLeftFront.setPower(MAX_CAP * multiplier);
+                motorLeftBack.setPower(MAX_CAP * multiplier);
+                motorRightFront.setPower(MAX_CAP * multiplier);
+                motorRightBack.setPower(MAX_CAP * multiplier);
+            }
+
+            for(DcMotor m : motorsWithChange) m.setDirection(fwd); // setting directions back to normal and we should be good
+
+        }
+    }
+
+    public void moveForward(double power){
+        DcMotor motors[] = {motorLeftFront, motorLeftBack, motorRightFront, motorRightBack};
+        DcMotorSimple.Direction leftDir = (leftRev) ? DcMotorSimple.Direction.REVERSE : DcMotorSimple.Direction.FORWARD;
+        DcMotorSimple.Direction rightDir = (leftRev) ? DcMotorSimple.Direction.FORWARD : DcMotorSimple.Direction.REVERSE;
+
+        motorLeftFront.setDirection(leftDir);
+        motorLeftBack.setDirection(leftDir);
+        motorRightFront.setDirection(rightDir);
+        motorRightBack.setDirection(rightDir);
+
+        for(DcMotor m : motors) m.setPower(power * multiplier);
+    }
+
+    public void moveBackward(double power){
+        moveForward(power);
+    }
+
+    public void moveLeft(double power){
+        
+    }
+
+    public void moveRight(double power){
+
     }
 
     @Override
     public void runOpMode(){
-        // Initialization
-        motorLeftBack = setMotor("motorLeftBack");
-        motorLeftFront = setMotor("motorLeftFront");
-        motorRightBack = setMotor("motorRightBack");
-        motorRightFront = setMotor("motorRightFront");
-
-        motorLeftBack.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorLeftFront.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
 
         while(opModeIsActive()){
-            update();
+            update(); // keeping data up to date
 
-            // changing some variables
-            if(gamepad1.dpad_left) decPower();
-            if(gamepad1.dpad_right) incPower();
-            if(gamepad.a) turnLeft();
+            // here are some control options by reversing booleans
+            if(gamepad1.a) leftRev = (leftRev) ? false:true;
+            if(gamepad1.back) suspendAllMotors = (suspendAllMotors) ? false:true;
 
-            if(lYAxis <= 1.0 || lYAxis >= -1.0 && lXAxis == 0.0) {
-                // moving forward or backward
-                motorLeftBack.setPower(-gamepad1.left_stick_y * multiplier);
-                motorLeftFront.setPower(-gamepad1.left_stick_y * multiplier);
-                motorRightBack.setPower(-gamepad1.left_stick_y * multiplier);
-                motorRightFront.setPower(-gamepad1.left_stick_y * multiplier);
-            }else if(lXAxis >= -1.0 && lXAxis < 0 && lYAxis == 0){
-                // moving left
-                motorLeftFront.setPower(-gamepad1.left_stick_x * multiplier);
-                motorRightBack.setPower(-gamepad1.left_stick_x * multiplier);
-            }else if(lXAxis <= 1.0 && lXAxis > 0 && lYAxis == 0){
-                // moving right
-                motorLeftBack.setPower(-gamepad1.left_stick_x * multiplier);
-                motorRightFront.setPower(-gamepad1.left_stick_x * multiplier)
-            }else{
-                if(lXAxis > lYAxis && lYAxis > 0 && testDiagonal) runDiagonal(Diagonal.RIGHT_FWRD);
-                else if(lXAxis < 0 && lYAxis > 0 && testDiagonal) runDiagonal(Diagonal.LEFT_FWRD);
-                else if(lXAxis > 0 && lYAxis < 0 && testDiagonal) runDiagonal(Diagonal.RIGHT_BACK);
-                else if(lXAxis < 0 && lYAxis > 0 && testDiagonal) runDiagonal(Diagonal.LEFT_BACK);
-                else // here we'll print a message to the driver theres a bad diagonal   
-            }
+            if(gamepad1.left_trigger > 0) turn('L');
+            else if(gamepad1.right_trigger > 0) turn('R');
+            else;
 
             idle();
         }
+    }
+
 }
